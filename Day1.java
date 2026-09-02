8fa91bcbc9c34094b0ddecd6f28408d79b7dc209
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-02 22:29:33 +0800

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,12 @@
+public class Day1 {
+    public static void main(String[] args) {
+        //ini adalah contoh single line
+
+        /*ini adalah contoh dari multiline */
+
+        /** lalu ini adalah contoh dari javadoc */
+
+        System.out.println("selamat belajar");
+    }
+    
+}
