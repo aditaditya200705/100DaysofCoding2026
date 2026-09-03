c71e47007fd079d02d60bcf872ee8cf35d7ffb51
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-03 18:21:19 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,33 @@
+public class Day2{
+    public static void main(String[] args){
+       //contoh println
+       System.out.println("nama saya ADITYA");
+       System.out.println("umur saya 19");
+
+       //println menampilkan teks dan pindah baris
+       
+
+       //contoh print
+       
+       System.out.print("saya berasal dari kabupaten mamasa");
+
+       //print menampilkan teks tanpa pindah baris
+
+       //contoh printf
+       System.out.printf("%nsaya dari prodi %s","INFORMATIKA");
+       System.out.printf("%ndan angkatan tahun %d",2026);
+
+       //catatan printf
+
+       /*%n digunakan untuk membuat baris baru 
+       %s adalah tempat untuk teks dan "ADITYA" adalah nilai yang dimasukkan
+       %d digunakan untuk bilangan bulat dan 2026 adalah nilainya*/
+    
+
+    }
+}
+
+       
+
+      
+    
