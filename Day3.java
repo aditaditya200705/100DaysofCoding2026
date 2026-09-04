7868caa6924f9ef170cf076eba8098295553b963
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-04 21:22:18 +0800

    Day3.java

--- Day3.java
+++ Day3.java
@@ -15,5 +15,22 @@ public class Day3 {
 
         System.out.println("Nama saya \"ADITYA\"");
         // \" digunakan untuk memunculkan tanda kutip
-    }
+
+        System.out.println("saya belajar \'java\'");
+        // \' untuk memunculkan petik satu
+
+        System.out.println("lokasi file: d:\\aditya\\100 DAYS CODING");
+        //menampilkan backslash
+
+        System.out.println("Saya sedang belajar javaX\b");
+        // \b untuk menghapus satu karakter sebelum tanda
+
+        System.out.println("Halaman 1\fHalaman 2");
+        // \f digunakan untuk pindah halaman
+
+        System.out.println("  halo\rhi");
+        // \r digunakan untuk membuat karakter setelah \r ke awal baris
+
+  }
 }
+
