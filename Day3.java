aa0c0705e6373c3e73720b6403cadd149da7fe81
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-04 15:34:09 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,19 @@
+public class Day3 {
+    
+    public static void main(String[] args){
+
+        System.out.println("BIODATA SAYA");
+
+        System.out.println("nama:ADITYA\nprodi:INFORMATIKA\nasal:MAMASA\n");
+         // \n digunakan untuk pindah baris
+
+
+        System.out.println("nama:\tADITYA");
+        System.out.println("prodi:\tinformatika\n");
+        // \t digunakan untuk membuat spasi
+
+
+        System.out.println("Nama saya \"ADITYA\"");
+        // \" digunakan untuk memunculkan tanda kutip
+    }
+}
