74cd1ad79f8590aa6f11683c9eff94b1f7451664
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-05 16:09:16 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,26 @@
+public class Day4 {
+    
+    public static void main(String[] args){
+        
+        //contoh tipe data byte
+
+        byte umur = 19;
+        System.out.println("umur" + umur);
+        // tipe data byte cocok untuk angka yang kecil dengan rentang niai -128 - 127
+
+        // contoh tipe data short misalnya jumlah penduduk suatu desa
+
+        short jumlahpenduduk= 2550;
+        System.out.println("jumlah penduduk desa A " + jumlahpenduduk  );
+        // digunakan untuk nilai angka sedang dengan rentang nilai -32.768 _ 32.767
+
+        // contoh tipe data int misalnya jumlah penduduk negara indonesia
+
+        int jumlahpendudukindonesia = (290125073);
+        System.out.println("jumlah penduduk indonesia saat ini " +jumlahpendudukindonesia);
+        //untuk data dengan angka besar dengan rentang nilai -2.147.483.648 - 2.147.483.647
+
+        // contoh tipe data long jumlah penduduk dunia 
+        long jumlahpendudukdunia = (8300000000l);
+        System.out.println("jumlah penduduk dunia saat ini " + jumlahpendudukdunia);
+        //untuk data dengan angka yang sangat besar bisa sampai 9,22 kuantiliun dan diakhiri dengan huruf l
