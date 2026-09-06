91c0f7f71f9d297d94fdd7bff2f2a217eb9b5f4d
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-06 18:27:50 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,20 @@
+public class Day5{
+
+    public static void main(String[] args) {
+        
+        //contoh tipe data double
+        double tinggi= 170.5;
+        double berat= 55.7;
+
+        System.out.println("tinggi anda adalah " + tinggi);
+        System.out.println("berat badan anda " + berat);
+
+         //contoh tipe data float
+         float nilaimatematika = 87.5f;
+
+         System.out.println("nilai matematika anda " + nilaimatematika);
+         /*keduanya digunakan untuk bilangan pecahan 
+         bedanya untuk float ditambah huruf "f" dibelakang nilainya*      
+
+    }
+}
