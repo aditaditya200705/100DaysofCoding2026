02bf22defc23cb96cbb07fe446f75db97e0ca41e
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-07 15:16:51 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,16 @@
+public class Day6{
+    public static void main(String[] args){
+        char hurufAwal = 'A' ;
+        //char hanya bisa menyimpan satu karakter dan menggunakan petik satu ('')
+
+        boolean punyaLaptop = true;
+        boolean punyaMobil = false;
+
+        //boolean hanya mempunyai dua kemungkinan nilai yaitu true dan false
+        //true berarti benar/ya dan false berarti salah/tidak
+
+        System.out.println(hurufAwal);
+        System.out.println(punyaLaptop);
+        System.out.println(punyaMobil);
+    }
+}
