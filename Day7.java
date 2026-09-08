2920f3e1384c4dc239286ef2e56ec7a4d1700e37
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-08 16:02:05 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,15 @@
+public class Day7 {
+    public static void main(String[] args){
+        String nama = "Aditya";
+        String alamat = "Mamasa";
+        String prodi = "Informatika";
+        //String adalah tipe data untuk memunculkan kalimat dan diberikan petik dua ("")
+
+        System.out.println("nama saya " + nama);
+        System.out.println("saya berasal dari " + alamat);
+        System.out.println("saya mengambil prodi " + prodi);
+
+
+
+    }
+}
