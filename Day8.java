35251cd0981b0695e86de845e24b7db4660e5013
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-09 19:49:35 +0800

    Update Day8.java

--- Day8.java
+++ Day8.java
@@ -1,1 +1,13 @@
+public class Day8{
+    public static void main(String[] args){
+        String nama; // ini adalah deklarasi yaitu memberi tipe data tapi nilai belum diberikan
 
+        nama = "Aditya"; //ini adalah inisialisasi dimana nilai sudah nilai sudah diberikan
+
+        System.out.println(nama);
+
+        nama = "Aditya informatika"; //ini adalah update dimana nilainya diubah
+
+        System.out.println(nama);
+    }
+}
