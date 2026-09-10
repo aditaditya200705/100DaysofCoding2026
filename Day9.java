2efc84ce9105bf8ff7eb40bf948231f476049417
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-10 20:12:35 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,11 @@
+public class Day9 {
+    public static void main(String[] args) {
+        String nama = "Aditya";
+        final int tahunLahir= 2007;
+        //final berarti nilai tersebut tidak dapat diubah lagi
+
+        System.out.println("nama = " + nama);
+        System.out.println("Tahun lahir = " + tahunLahir);
+    }
+    
+}
