62b6665ae5d9a21b45e5df56524a8432523a236c
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-13 13:10:50 +0800

    Create Day12.java

---
+++ Day12.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Day12 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Nama: ");
+        String nama = input.nextLine();
+
+        System.out.print("Umur: ");
+        int umur = input.nextInt();
+
+        System.out.print("Asal: ");
+        String asal = input.nextLine();
+
+        System.out.print("Prodi: ");
+        String prodi = input.nextLine();
+
+        System.out.println("=== BIODATA ===");
+        System.out.println("Nama  : " + nama);
+        System.out.println("Umur  : " + umur);
+        System.out.println("Asal  : " + asal);
+        System.out.println("Prodi : " + prodi);
+    }
+}
