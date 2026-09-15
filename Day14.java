8da39f56c8ada3e0a5f0862f3a27f995da508f1f
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-15 20:49:54 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,9 @@
+public class Day14{
+    public static void main(String[] args) {
+        int angka1 = 15;
+        int angka2 = 10;
+
+        System.out.println("hasil penjumlahan: " + (angka1 + angka2));
+        System.out.println("hasil pengurangan: " + (angka1 - angka2));
+    }
+}
