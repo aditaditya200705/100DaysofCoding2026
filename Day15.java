4f5bc252120605cd9096c985622ca90578cfbf99
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-16 19:20:58 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,10 @@
+public class Day15{
+    public static void main(String[] args) {
+
+        int A = 25;
+        int B = 5;
+        
+        System.out.println("Hasil Pembagian: " + (A / B));
+        System.out.println("Hasil Perkalian: " + (A * B));
+    }
+}
