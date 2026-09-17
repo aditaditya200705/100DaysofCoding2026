47f6446dc190e728ef6862a4a67542b05603fd73
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-17 19:07:00 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,12 @@
+public class Day16 {
+    public static void main(String[] args) {
+        
+        int A = 13;
+        int B = 2;
+
+        System.out.println("hasil pembagian: " +(A/B));
+        System.out.println("sisa pembagian: " + (A%B));
+
+    }
+
+}
