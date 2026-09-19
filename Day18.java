a6739ea18c552df0bb208c384374f924767e143c
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-19 23:14:41 +0800

    Update Day18.java

--- Day18.java
+++ Day18.java
@@ -6,12 +6,17 @@ public class Day18 {
 
         System.out.print("Masukkan umur: ");
         byte umur = input.nextByte();
-        int umur1 = umur;
-        double umur2 = umur1;
+        short umur1 = umur;
+        int umur2 = umur1;
+        long umur3 = umur2;
+        float umur4 = umur3;
+        double umur5 = umur4;
 
         System.out.println("umur : " + umur);
-        System.out.println("umur int: " + umur1 );
-        System.out.println("umur double :" + umur2);
-        input.close();
+        System.out.println("umur short : " + umur1 );
+        System.out.println("umur int :" + umur2);
+        System.out.println("umur long :" + umur3);
+        System.out.println("umur float :" + umur4);
+        System.out.println("umur double :" + umur5);        
     }  
 }
