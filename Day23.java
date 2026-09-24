dd92970f8e17a44d4809ec9f7da0c760ae115ac1
aditaditya200705 <aditaditya200705@gmail.com>
2026-09-24 20:11:25 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Day23 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang sisi: ");
+        int sisi = input.nextInt();
+
+        int luas = sisi * sisi;
+
+        System.out.println("Luas persegi: " + luas);
+
+        input.close();
+    }
+}
